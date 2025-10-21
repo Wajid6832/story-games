7de242db34182c519417b604bcc4a8cdf2f52b9a
Hussnain1238 <personalhussnain@gmail.com>
2025-10-21 20:35:44 +0500

    landingpage-sidebar-Homepage

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./features/auth/Login";
 import { Products } from "./features/product/Product";
+import LandingPage from "./components/common/Landing/LandingPage";
+import WriterHome from "./components/Pages/WriterHome";
+import WriterSidebar from "./components/Sidebar/WriterSidebar"
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <>
+     <div className="App">
+      {/* <BrowserRouter>
       <Routes>
         <Route path="/products" element={<Products />} />
         <Route path="/" element={<Login />} />
       </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      
+      <LandingPage/>
+        <WriterHome />
+      
     </div>
+    </>
+   
   );
 }
 
---
+++ src/components/common/Landing/LandingPage.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import image from "../../../assets/1stimage.png"
+import styles from "../../../Modulecss/Writerlanding.module.css"
+
+const LandingPage = () => {
+    const footer=[
+          1,2,3,4,5,6
+    ]
+        
+  return (
+    <div className={styles.landingWrapper}>
+      <Container fluid className="px-5 py-5">
+        <Row className={styles.main}>
+          <Col md={6}>
+            <h1 className="fw-bold mb-3">Story Host</h1>
+            <div className= {styles.line} >
+                <p className="text-secondary fs-5 mb-0">
+              Lorem ipsum dolor sit amet
+            </p> <div className={styles.line2}></div>
+            </div> 
+            <p className="text-muted mb-4" >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            </p>
+            <div className="d-flex gap-3 mb-3">
+              <Button variant="primary" className={styles.signUpBtn}>
+                Sign Up
+              </Button>
+              <button className={styles.signinBtn}>Sign In</button> <div></div>
+            </div>
+            <a href="#" className={styles.guestLink}>
+              Browse as Guest
+            </a>
+          </Col>
+        <Col md={6} className="text-center">
+            <img
+              src={image}
+              alt="Books Illustration"
+              className={styles.heroImage}
+            />
+          </Col>
+        </Row>
+        <div className={styles.footer}>
+          <Row className="align-items-center ">
+            <Col xs={9}>
+              <Row className="g-3">
+                {footer.map((item) => (
+                  <Col key={item} xs={6} sm={4} md={2}>
+                    <Card className={styles.placeholderCard}>
+                      <i className="bi bi-image"></i>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Col>
+            <Col  className="text-start">
+              <h3 className="fw-semibold mt-2"  >Top lists</h3>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </div>
+  );
+};
+export default LandingPage;
+
+
+
+
+
+
+
+
+
