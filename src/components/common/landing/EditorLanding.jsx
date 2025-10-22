f08a700f4630b78a2802a7b32979f12e2b9437f4
shahzadahmad7489879 <shahzadahmad7489879@gmail.com>
2025-10-22 20:01:31 +0500

    chnage css simple to modular css

--- src/components/common/landing/EditorLanding.jsx
+++ src/components/common/landing/EditorLanding.jsx
@@ -1,78 +1,74 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./EditorLanding.css";
-import Frame from "../../../assets/Frame.png"
+import styles from "./EditorLanding.module.css";
+import Frame from "../../../assets/Frame.png";
 import { useNavigate } from "react-router-dom";
 
 const EditorLanding = () => {
   const navigator = useNavigate();
+
   return (
-    <div className="main-landing-div">
-      <div className="landing-second-div">
-        <div className="lading-content-div">
-
-          <div className="landign-content">
-
-            <div className="content-upper">
-
-              <div className="content">
-                <div className="heading">
+    <div className={styles.mainLandingDiv}>
+      <div className={styles.landingSecondDiv}>
+        <div className={styles.ladingContentDiv}>
+          <div className={styles.landignContent}>
+            <div className={styles.contentUpper}>
+              <div className={styles.content}>
+                <div className={styles.heading}>
                   <p>STORY HOST</p>
                 </div>
-                <div className="second-heading">
+                <div className={styles.secondHeading}>
                   <p>Lorem ipsum dolor it amet</p>
-                  <div className="dasht-line"></div>
+                  <div className={styles.dashtLine}></div>
                 </div>
-                <div className="text">
-                  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                    sed diam nonummy nibh euismod
-                    tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+                <div className={styles.text}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                    magna aliquam erat volutpat
+                  </p>
                 </div>
-                <div className="butons">
-                  <div className="btn">
-                    <div className="signup">
+                <div className={styles.butons}>
+                  <div className={styles.btn}>
+                    <div className={styles.signup}>
                       <button>Signup</button>
                     </div>
-                    <div className="signin">
+                    <div className={styles.signin}>
                       <button>SignIN</button>
                     </div>
                   </div>
                 </div>
-                <div className="browse">
+                <div className={styles.browse}>
                   <p>Browse as guest</p>
                 </div>
               </div>
 
-              <div className="image">
+              <div className={styles.image}>
                 <img src={Frame} alt="" />
               </div>
-
-            </div>
-
-
-
-          </div>
-
-          <div className="top-list-container">
-            <div className="card-list">
-              <div className="lower-setction-cards">
-                {Array(6).fill(0).map((_, index) => (
-                  <div className="card" key={index}>
-                    <img src="placeholder.png" alt="Placeholder" />
-                  </div>
-                ))}
-              </div>
-
-              <div className="top-list-label">
-                <button onClick={()=>navigator("home")}>Top List</button>
-              </div>
             </div>
           </div>
 
+          <div className={styles.topListContainer}>
+            <div className={styles.cardList}>
+              <div className={styles.lowerSetctionCards}>
+                {Array(6)
+                  .fill(0)
+                  .map((_, index) => (
+                    <div className={styles.card} key={index}>
+                      <img src="placeholder.png" alt="Placeholder" />
+                    </div>
+                  ))}
+              </div>
+
+              <div className={styles.topListLabel}>
+                <button onClick={() => navigator("home")}>Top List</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default EditorLanding
+export default EditorLanding;
--- src/components/common/sidebar/EditorSidebar.jsx
+++ src/components/common/sidebar/EditorSidebar.jsx
@@ -1,201 +1,161 @@
-import React, { useState } from 'react'
-import Logo from "../../../assets/logo.png"
-import Menue from "../../../assets/menu.png"
-import Profile from "../../../assets/profile.png"
-import Novels from "../../../assets/novels.png"
-import Producer from "../../../assets/producer.png"
-import Application from "../../../assets/applications.png"
-import Favourites from "../../../assets/favourites.png"
-import Mail from "../../../assets/mail.png"
-import Logout from "../../../assets/logout.png"
-import style from "./EditorSidebar.module.css"
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import Logo from "../../../assets/logo.png";
+import Menue from "../../../assets/menu.png";
+import Profile from "../../../assets/profile.png";
+import Novels from "../../../assets/novels.png";
+import Producer from "../../../assets/producer.png";
+import Application from "../../../assets/applications.png";
+import Favourites from "../../../assets/favourites.png";
+import Mail from "../../../assets/mail.png";
+import Logout from "../../../assets/logout.png";
+import styles from "./EditorSidebar.module.css";
+import { NavLink } from "react-router-dom";
+
 const EditorSidebar = () => {
-    const [isOpen, SetIsopen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleOpen = () => {
-        SetIsopen(!isOpen);
-    }
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
-    if (!isOpen) {
-        return (
-            <div className='shoe-menue'>
-                <div className="manue">
-                    <button onClick={handleOpen}>
-                        <img src={Menue} alt="menue" />
-                    </button>
+  if (!isOpen) {
+    return (
+      <div className={styles.shoeMenue}>
+        <div className={styles.menue}>
+          <button onClick={handleOpen}>
+            <img src={Menue} alt="menu" />
+          </button>
+        </div>
+        <div className={styles.shoeManueLinks}>
+          {[Novels, Producer, Application, Favourites, Mail].map((icon, index) => (
+            <NavLink key={index} className={styles.navlinks}>
+              <div className={styles.links}>
+                <div className={styles.imgLinks}>
+                  <img src={icon} alt="icon" />
                 </div>
-                <div className="shoe-manue-links">
-                    <NavLink className="navlinks">
-                        <div className="links">
-                            <div className="img-links">
-                                <img src={Novels} alt="Profile" />
-                            </div>
+              </div>
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-                        </div>
-                    </NavLink>
-
-                    <NavLink className="navlinks">
-                        <div className="links">
-                            <div className="img-links">
-                                <img src={Producer} alt="Profile" />
-                            </div>
-
-                        </div>
-                    </NavLink>
-
-                    <NavLink className="navlinks">
-                        <div className="links">
-                            <div className="img-links">
-                                <img src={Application} alt="Profile" />
-                            </div>
-                        </div>
-                    </NavLink>
-
-                    <NavLink className="navlinks">
-                        <div className="links">
-                            <div className="img-links">
-                                <img src={Favourites} alt="Profile" />
-                            </div>
-                        </div>
-                    </NavLink>
-
-                    <NavLink className="navlinks">
-                        <div className="links">
-                            <div className="img-links">
-                                <img src={Mail} alt="Profile" />
-                            </div>
-                        </div>
-                    </NavLink>
-                </div>
-
+  return (
+    <div className={styles.sidebarContainer}>
+      <div className={styles.upperContainer}>
+        <div className={styles.storyhostManue}>
+          <div className={styles.sotryBook}>
+            <div className={styles.img}>
+              <img src={Logo} alt="logo" />
             </div>
-        )
-    }
-    if (isOpen) {
-        return (
-            <div className='sidebar-container'>
-                <div className="container upper-container">
-                    <div className="upper-conent">
-                        <div className="storyhost-manue">
-                            <div className="sotry-book d-flex justify-content-between align-content-center">
-                                <div className="img">
-                                    <img src={Logo} alt="logo" />
-                                </div>
-                                <div className='story-text'>
-                                    <p>Story Host</p>
-                                </div>
-                            </div>
-                            <div className="manue">
-                                <button onClick={handleOpen}>
-                                    <img src={Menue} alt="menue" />
-                                </button>
-                            </div>
-                        </div>
-                        <div className="links-list">
-                            <NavLink className="navlinks">
-                                <div className="links">
-                                    <div className="img-links">
-                                        <img src={Profile} alt="Profile" />
-                                    </div>
-                                    <div className={style.linkText}>
-                                        <p>Profile</p>
-                                    </div>
-                                </div>
-                            </NavLink>
-
-
-
-                            <NavLink className="navlinks">
-                                <div className="links">
-                                    <div className="img-links">
-                                        <img src={Novels} alt="Profile" />
-                                    </div>
-                                    <div className="link-text">
-                                        <p>Current Novels</p>
-                                    </div>
-                                </div>
-                            </NavLink>
-
-                            <NavLink className="navlinks">
-                                <div className="links">
-                                    <div className="img-links">
-                                        <img src={Producer} alt="Profile" />
-                                    </div>
-                                    <div className="link-text">
-                                        <p>Become a Producer</p>
-                                    </div>
-                                </div>
-                            </NavLink>
-
-                            <NavLink className="navlinks">
-                                <div className="links">
-                                    <div className="img-links">
-                                        <img src={Application} alt="Profile" />
-                                    </div>
-                                    <div className="link-text">
-                                        <p>Open Application</p>
-                                    </div>
-                                </div>
-                            </NavLink>
-
-                            <NavLink className="navlinks">
-                                <div className="links">
-                                    <div className="img-links">
-                                        <img src={Favourites} alt="Profile" />
-                                    </div>
-                                    <div className="link-text">
-                                        <p>Favorites</p>
-                                    </div>
-                                </div>
-                            </NavLink>
-
-                            <NavLink className="navlinks">
-                                <div className="links">
-                                    <div className="img-links">
-                                        <img src={Mail} alt="Profile" />
-                                    </div>
-                                    <div className="link-text">
-                                        <p>Messages</p>
-                                    </div>
-                                </div>
-                            </NavLink>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="container lower-container">
-                    <div className="common-lowercontent mode-on-of">
-                        <div className="form-check form-switch">
-                            <input
-                                className="form-check-input"
-                                type="checkbox"
-                                role="switch"
-                                id="switchCheckDefault"
-                                defaultChecked
-                            />
-                            <label className="form-check-label" htmlFor="switchCheckDefault">
-                                Read mode of
-                            </label>
-                        </div>
-
-                    </div>
-                    <div className="common-lowercontent suprot">
-                        <NavLink>
-                            <div className='d-flex'>
-                                <img src={Logout} alt="Logout" />
-                                <p>Logout</p>
-                            </div>
-                        </NavLink>
-                    </div>
-                    <div className="common-lowercontent sign-out">
-
-                    </div>
-                </div>
-
+            <div className={styles.storyText}>
+              <p>Story Host</p>
             </div>
-        )
-    }
-}
+          </div>
+          <div className={styles.menue}>
+            <button onClick={handleOpen}>
+              <img src={Menue} alt="menu" />
+            </button>
+          </div>
+        </div>
 
-export default EditorSidebar
+        <div className={styles.linksList}>
+          <NavLink className={styles.navlinks}>
+            <div className={styles.links}>
+              <div className={styles.imgLinks}>
+                <img src={Profile} alt="Profile" />
+              </div>
+              <div className={styles.linkText}>
+                <p>Profile</p>
+              </div>
+            </div>
+          </NavLink>
+
+          <NavLink className={styles.navlinks}>
+            <div className={styles.links}>
+              <div className={styles.imgLinks}>
+                <img src={Novels} alt="Current Novels" />
+              </div>
+              <div className={styles.linkText}>
+                <p>Current Novels</p>
+              </div>
+            </div>
+          </NavLink>
+
+          <NavLink className={styles.navlinks}>
+            <div className={styles.links}>
+              <div className={styles.imgLinks}>
+                <img src={Producer} alt="Become a Producer" />
+              </div>
+              <div className={styles.linkText}>
+                <p>Become a Producer</p>
+              </div>
+            </div>
+          </NavLink>
+
+          <NavLink className={styles.navlinks}>
+            <div className={styles.links}>
+              <div className={styles.imgLinks}>
+                <img src={Application} alt="Application" />
+              </div>
+              <div className={styles.linkText}>
+                <p>Open Application</p>
+              </div>
+            </div>
+          </NavLink>
+
+          <NavLink className={styles.navlinks}>
+            <div className={styles.links}>
+              <div className={styles.imgLinks}>
+                <img src={Favourites} alt="Favorites" />
+              </div>
+              <div className={styles.linkText}>
+                <p>Favorites</p>
+              </div>
+            </div>
+          </NavLink>
+
+          <NavLink className={styles.navlinks}>
+            <div className={styles.links}>
+              <div className={styles.imgLinks}>
+                <img src={Mail} alt="Messages" />
+              </div>
+              <div className={styles.linkText}>
+                <p>Messages</p>
+              </div>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+
+      <div className={styles.lowerContainer}>
+        <div className={`${styles.commonLowercontent} ${styles.modeOnOf}`}>
+          <div className="form-check form-switch">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="switchCheckDefault"
+              defaultChecked
+            />
+            <label className="form-check-label" htmlFor="switchCheckDefault">
+              Read mode off
+            </label>
+          </div>
+        </div>
+
+        <div className={`${styles.commonLowercontent} ${styles.suport}`}>
+          <NavLink>
+            <div className={styles.logoutWrapper}>
+              <img src={Logout} alt="Logout" />
+              <p>Logout</p>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditorSidebar;
