c3b5618d27a6c0ae28a81b07452a310e694b2aa4
Sufyan Ali <166269513+SufyanAli-04@users.noreply.github.com>
2025-10-22 20:33:00 +0500

    chat-box complete

--- src/App.js
+++ src/App.js
@@ -5,18 +5,58 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./features/auth/Login";
 import { Products } from "./features/product/Product";
+import WriterLanding from "./components/Common/Landing/WriterLanding";
+import Sidebar from "./components/Sidebar/WriterSidebar.jsx";
+import WriterHome from "./components/Pages/WriterHome.jsx";
+import LandingPage from "./components/LandingPage.jsx";
+import ChatApp from "./components/Pages/ChatApp.jsx";
+
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
       <Routes>
         <Route path="/products" element={<Products />} />
         <Route path="/" element={<Login />} />
       </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+
+      {/* <WriterLanding/> */}
+{/* <LandingPage/> */}
+      <ChatApp />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import { Button } from "react-bootstrap";
+// // import NewChatModal from "./NewChatModal";
+// import NewChatModal from "./components/Modal/NewChatModal";
+
+// const ChatPage = () => {
+//   const [show, setShow] = useState(false);
+
+//   return (
+//     <div className="p-5 text-center">
+//       <Button variant="primary" onClick={() => setShow(true)}>
+//         Open New Chat Modal
+//       </Button>
+
+//       <NewChatModal show={show} handleClose={() => setShow(false)} />
+//     </div>
+//   );
+// };
+
+// export default ChatPage;
