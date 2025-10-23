903ad8b3a46c613bf5846b0b84dd31be9e2d89b3
Hussnain1238 <personalhussnain@gmail.com>
2025-10-23 14:09:53 +0500

    resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "./features/auth/Login";
-import { Products } from "./features/product/Product";
-import WriterHome  from "./components/Pages/WriterHome";
-// import EditorLanding from "./components/common/landing/EditorLanding";
-=======
+
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -15,7 +8,6 @@ import { Products } from "./features/product/Product";
 // Writer/Editor Components
 import WriterHome from "./components/Pages/WriterHome";
 import EditorLanding from "./components/common/landing/EditorLanding";
->>>>>>> daa3d06c3d05c1fe77ebcabba2e3e2bd0b005153
 import Home from "./components/common/home/Home";
 import Bookpage from "./components/Pages/Bookpages/Bookpage";
 
@@ -29,20 +21,13 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-<<<<<<< HEAD
       <Routes>
         {/* <Route path="/products" element={<Products />} />
         <Route path="/" element={<Login />} /> */}
         <Route path="/WriterHome" element={<WriterHome />} />
         <Route path="/bookpage" element={<Bookpage />} />
-        {/* <Route path="/" element={<EditorLanding/>} /> */}
-        <Route path="/home" element={<Home/>} />
-      </Routes>
-=======
-        <Routes>
-          {/* Common / Auth Routes */}
-          {/* <Route path="/products" element={<Products />} /> */}
-          {/* <Route path="/" element={<Login />} /> */}
+     
+   
 
           {/* Writer & Editor Routes */}
           <Route path="/writer" element={<WriterHome />} />
@@ -55,7 +40,6 @@ function App() {
           <Route path="/currentnovels" element={<CurrentNovels />} />
           <Route path="/supportfeedback" element={<SupportFeedback />} />
         </Routes>
->>>>>>> daa3d06c3d05c1fe77ebcabba2e3e2bd0b005153
       </BrowserRouter>
     </div>
   );
