59e574c64ac7996777079658a82fdd6a1b831da5
Hussnain1238 <personalhussnain@gmail.com>
2025-10-23 13:04:15 +0500

    Resolved merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./features/auth/Login";
 import { Products } from "./features/product/Product";
 import WriterHome  from "./components/Pages/WriterHome";
-import EditorLanding from "./components/common/landing/EditorLanding";
+// import EditorLanding from "./components/common/landing/EditorLanding";
 import Home from "./components/common/home/Home";
+import Bookpage from "./components/Pages/Bookpages/Bookpage";
 
 
 function App() {
@@ -17,8 +16,9 @@ function App() {
       <Routes>
         {/* <Route path="/products" element={<Products />} />
         <Route path="/" element={<Login />} /> */}
-        <Route path="/" element={<WriterHome />} />
-        <Route path="/" element={<EditorLanding/>} />
+        <Route path="/WriterHome" element={<WriterHome />} />
+        <Route path="/bookpage" element={<Bookpage />} />
+        {/* <Route path="/" element={<EditorLanding/>} /> */}
         <Route path="/home" element={<Home/>} />
       </Routes>
       </BrowserRouter>
