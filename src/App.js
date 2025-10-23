1c72b56782438d0c4b175bf78fa6442738e505ce
Khalil <khalilqaisrani1215@gmail.com>
2025-10-23 19:57:59 +0500

    group chat and page make

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./features/auth/Login";
 import { Products } from "./features/product/Product";
@@ -11,33 +11,38 @@ import ReadersLanding from "./components/Common/Landing/ReadersLanding";
 import CurrentNovels from "./components/Common/Landing/CurrentNovels";
 import SupportFeedback from "./components/Common/Landing/SupportFeedback"; 
 
-
-import React from 'react'
 import Create from './component/Create'
-// import PageReducer from './component/Loadingpage/PageReducer'
+import BookName from "../src/component/Loadingpage/Book";
+// import Navbar from './component/Loadingpage/Navbar';
+import PageReducer from './component/Loadingpage/PageReducer'
+import Book from "../src/component/Loadingpage/Book";
 const App = () => {
   return (
 
     <div>
       {/* <PageReducer /> */}
-      <Create />
+      {/* <Create /> */}
+      {/* <Navbar /> */}
 
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
       <Routes>
-        {/* <Route path="/products" element={<Products />} /> */}
-        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<Login />} />
         <Route path="/" element={<ReaderHome/>} />
         <Route path="/readinghome" element={<ReadersLanding/>} />
         <Route path="/CurrentNovels" element={<CurrentNovels />} />
         <Route path="/SupportFeedback" element={<SupportFeedback />} /> 
       </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Book/>
 
-    </div>
-    </div>
-  )
-}
+    </div> 
+     </div>
+   )
+ }
 
 
 export default App;
+
+
---
+++ src/component/Loadingpage/Book.jsx
@@ -0,0 +1,77 @@
+
+
+
+import React from "react";
+import styles from './Book.module.css';
+import Group2 from '../../assets/img1/Group2.png'
+import img2 from '../../assets/img1/images.png'
+import write from "../../assets/img1/write.png"
+import search from '../../assets/img1/search.png'
+import fram from '../../assets/img1/Frame 512783.png'
+const Book = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles.header}>
+          <div className={styles.iconSection}>
+            <div className={styles.chatIcon}>
+              <div className={styles.message}></div>
+            </div>
+            <div className={styles.userIcons}>
+              <div className={styles.user}></div>
+              <div className={styles.user}></div>
+            </div>
+          </div>
+          <div>
+            <img src={fram} alt="" className={styles.fram1}/>
+          </div>
+           <img src={Group2} alt="" className={styles.img2}/>
+          <h2 className={styles.title}>Group Chat</h2>
+        </div>
+
+        <div className={styles.uploadSection}>
+          <div className={styles.uploadCircle}>
+            
+            <img src={img2} alt="Upload" className={styles.uploadIcon} />
+          </div>
+          
+          <p>Upload Group Photo</p>
+        </div>
+
+        <div className={styles.form}>
+     <div className={styles.inputContainer}>
+          <img src={write} alt="icon" className={styles.inputImg} />
+          <input
+            type="text"
+            placeholder="Create Group Name"
+            className={styles.inputField}
+          />
+        </div>
+       
+          <div className={styles.textareaContainer}>
+  <img src={write} alt="icon" className={styles.icon3} />
+  <textarea
+    placeholder="Create Group Description"
+    className={styles.textarea}
+  ></textarea>
+</div>
+          <div className={styles.inputContainer}>
+  <img src={search} alt="search icon" className={styles.searchIcon} />
+  <input
+    type="text"
+    placeholder="Search to Add Group Members"
+    className={styles.input}
+  />
+</div>
+
+          <button className={styles.createButton}>Create Group</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
+
+
+
