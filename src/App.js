01c5508a4000517209d91abfe484e8bbdc0877b4
wajid <chwajid6832@gmail.com>
2025-10-23 10:12:11 +0500

    create landing page and workroompage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
+// import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./features/auth/Login";
 import { Products } from "./features/product/Product";
+import WorkRoomPage from "./components/WorkRoom/WorkRoomPage";
+import LandingPage from "./components/Landing/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
       <Routes>
         <Route path="/products" element={<Products />} />
         <Route path="/" element={<Login />} />
       </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      {/* <WorkRoomPage/> */}
+      <LandingPage/>
     </div>
   );
 }
