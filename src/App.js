b5e26c703223f9c83d60b0aad3cb3fc188df72a3
Khalil <khalilqaisrani1215@gmail.com>
2025-10-29 17:34:24 +0500

    Saved my App.js changes before merging staging

--- src/App.js
+++ src/App.js
@@ -4,48 +4,46 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import  Landing  from "./components/Common/Landing/EditorLanding";
+// import { Products } from "./features/product/Product";
+import { Login } from "./features/auth/Login";
+import Book2 from "./components/Pages/ProducerOnlyComponents/Book2/Book2";
+import Book3 from "./components/Pages/ProducerOnlyComponents/Book3/Book3";
+import Book from "./components/Pages/ProducerOnlyComponents/Book/Book";
+import NewChat from "./components/Pages/ProducerOnlyComponents/New Chat/NewChat";
+import NewChat2 from "./components/Pages/ProducerOnlyComponents/New Chat2/NewChat2";
+import PageReducer from "./components/Pages/ProducerOnlyComponents/Page Reducer/PageReducer";
+import Create from "./components/Pages/ProducerOnlyComponents/Create/Create";
 
-// import Create from './component/Create'
-// import BookName from "../src/component/Loadingpage/Book";
-// import Navbar from './component/Loadingpage/Navbar';
-// import PageReducer from './component/Loadingpage/PageReducer'
-// import NewChat from "./component/Loadingpage/NewChat";
-// import NewChat2 from './component/Loadingpage/NewChat2'
-// import Book from "../src/component/Loadingpage/Book";
-    import Book2 from '../src/component/Loadingpage/Book2'
-// import Book3 from './component/Loadingpage/Book3'
-// import MyProfile from './component/Loadingpage/MyProfile'
+
+
+
+
 const App = () => {
   return (
 
-    
+
 
 
     <div className="App">
-      {/* <BrowserRouter>
-      <Routes>
-        <Route path="/products" element={<Products />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/" element={<ReaderHome/>} />
-        <Route path="/readinghome" element={<ReadersLanding/>} />
-        <Route path="/CurrentNovels" element={<CurrentNovels />} />
-        <Route path="/SupportFeedback" element={<SupportFeedback />} /> 
-        <Route path="/" element={<Landing/>} />
-      </Routes>
-      </BrowserRouter> */}
-      <Book2/>
-      {/* <Book3 /> */}
-      {/* <MyProfile /> */}
-      {/* <NewChat /> */}
-      {/* <NewChat2 /> */}
-       {/* <PageReducer /> */}
-      {/* <Create /> */}
-      {/* <Navbar /> */}
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/products" element={<Products />} /> */}
+          <Route path="/" element={<Login />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/book2" element={<Book2 />} />
+          <Route path="/book3" element={<Book3 />} />
+          <Route path="/newchat" element={<NewChat />} />
+          <Route path="/newchat2" element={<NewChat2 />} />
+          <Route path="/pagereducer" element={<PageReducer />} />
+          <Route path="/create" element={<Create />} />
+         
+        </Routes>
+      </BrowserRouter>
 
-    </div> 
-   )
- }
+
+    </div>
+  )
+}
 
 export default App;
 
