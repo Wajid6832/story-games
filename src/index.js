f6052c6e10ff26a2749018434825aee752f2edb3
unknown <Muhammad Wajid>
2025-11-04 20:23:34 -0800

    resolve conflicts and updated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
