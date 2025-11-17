66310793b3e5f6aef5ad6dc4999a49e37eb5a260
unknown <Muhammad Wajid>
2025-11-17 13:00:48 -0800

    after testing all code

--- story-games/src/components/Pages/ProducerOnlyComponents/Create/Create.jsx
+++ story-games/src/components/Pages/ProducerOnlyComponents/Create/Create.jsx
@@ -1,70 +1,54 @@
-import React, { useState } from 'react'
-import styles from './Create.module.css'
-
-import Group from '../../../../assets/Group.png'
-import write from '../../../../assets/write.png'
-import document from '../../../../assets/document.png'
+import React, { useState } from 'react';
+import styles from './Create.module.css';
+import Group from '../../../../assets/Group.png';
+import write from '../../../../assets/write.png';
+import document from '../../../../assets/document.png';
 import { RxCrossCircled } from "react-icons/rx";
-const Create = () => {
-  //  const [data , setData] = useState({
-    
-  //  });
 
-  const [data, setData] = useState({
-    submissionName: ""
-  });
+const Create = ({onClose }) => {
+  const [data, setData] = useState({ submissionName: "" });
 
-  
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setData({ submissionName: value });
-    console.log("Submission Name:", value);
-  };
+  const handleChange = (e) => setData({ submissionName: e.target.value });
 
 
   return (
-    <div className={styles.createContainer}>
-      <div >
-      <RxCrossCircled className={styles.crossIcon} />
-      </div>
-      <div className={styles.illustration}>
-        <img src={Group} alt="illustration" />
-      </div>
+    <div className={styles.modalOverlay} onClick={onClose}>
+      <div className={styles.modalBox} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.createContainer}>
+          <RxCrossCircled className={styles.crossIcon} onClick={onClose} />
 
-      <h2 className={styles.title}>Create Submission</h2>
-      <div className={styles.inputContainer}>
-        <img src={write} alt="icon" className={styles.inputImg} />
-        <input
-          type="text"
-           onChange={handleChange}
-          value={data.submissionName}
-          placeholder="Create Submission Name"
-          className={styles.inputField}
-        />
-      </div>
-
-      <button className={styles.uploadBtn}>Upload Adventure Log From Writer</button>
-
-      <h4 className={styles.uploadedTitle}>Uploaded Document</h4>
-
-      <div className={styles.uploadedBox} style={{position: "relative"}}>
-        <p>
-          <img src={document} alt="" className={styles.documentImg} />
-          <div className={styles.text}>
-
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo.
+          <div className={styles.illustration}>
+            <img src={Group} alt="illustration" />
           </div>
-        </p>
+
+          <h2 className={styles.title}>Create Submission</h2>
+
+          <div className={styles.inputContainer}>
+            <img src={write} alt="icon" className={styles.inputImg} />
+            <input
+              type="text"
+              onChange={handleChange}
+              value={data.submissionName}
+              placeholder="Create Submission Name"
+              className={styles.inputField}
+            />
+          </div>
+
+          <button className={styles.uploadBtn}>Upload Adventure Log From Writer</button>
+
+          <h4 className={styles.uploadedTitle}>Uploaded Document</h4>
+          <div className={styles.uploadedBox}>
+            <img src={document} alt="" className={styles.documentImg} />
+            <div className={styles.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            </div>
+          </div>
+
+          <button className={styles.btn}>Submit Submission</button>
+        </div>
       </div>
-
-      <button className={styles.btn}>Submit Submission</button>
     </div>
-
-  )
-}
+  );
+};
 
 export default Create;
-
